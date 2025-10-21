80d9698c3177ec3a950e2492465f942a2b6f8b4b
yashvasudeva1 <117675995+yashvasudeva1@users.noreply.github.com>
2025-10-21 10:26:26 +0530

    Update app.py

--- Decision Tree/app.py
+++ Decision Tree/app.py
@@ -3,7 +3,7 @@ import joblib
 from io import StringIO
 model=joblib.load('Decision Tree/Decision_Tree.pkl')
 st.title('Decision Tree Classifier')
-tab1,tab2=st.tabs(['Unhyperparameterized','Hyperparameterized with Postpruning', 'Hyperparameterized with Prepruning'])
+tab1,tab2,tab3=st.tabs(['Unhyperparameterized','Hyperparameterized with Postpruning', 'Hyperparameterized with Prepruning'])
 with tab1:
     st.header('Unhyperparameterized')
     sepal_length=st.number_input('sepal_length',0.0,10.0)
@@ -39,5 +39,6 @@ with tab1:
           buffer.write("\n" + "="*40 + "\n")
           buffer.write("End of report\n")
           text_content = buffer.getvalue()
+
 
 
